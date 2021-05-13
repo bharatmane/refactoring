dbbd140431cd5603a6a471a381a95f2f3d24ee8e
bharat.mane <bharat.mane@outlook.com>
2021-05-13 19:13:28 +0530

    feat: Initial class structure

---
+++ src/Chapter1/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter1
+{
+    public class Movie
+    {
+        public const int CHILDREN = 2;
+        public const int REGULAR = 0;
+        public const int NEW_RELEASE = 1;
+
+        private String title;
+        private int priceCode;
+
+        public Movie(String title, int priceCode)
+        {
+            this.title = title;
+            this.priceCode = priceCode;
+        }
+
+        public String GetTitle()
+        {
+            return title;
+        }
+
+        public int GetPriceCode()
+        {
+            return priceCode;
+        }
+
+        public void SetPriceCode(int priceCode)
+        {
+            this.priceCode = priceCode;
+        }
+    }
+}
---
+++ src/Chapter1/Rental.cs
@@ -0,0 +1,24 @@
+﻿namespace Chapter1
+{
+    public class Rental
+    {
+        private Movie movie;
+        private int daysRented;
+
+        public Rental(Movie movie, int daysRented)
+        {
+            this.movie = movie;
+            this.daysRented = daysRented;
+        }
+
+        public Movie GetMovie()
+        {
+            return movie;
+        }
+
+        public int GetDaysRented()
+        {
+            return daysRented;
+        }
+    }
+}
