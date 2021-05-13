efc76916b989b61b7c1f8d8c0706f29628ea8f13
bharat.mane <bharat.mane@outlook.com>
2021-05-14 00:08:17 +0530

    fix: fixed the failing test case

--- src/Chapter1/Program.cs
+++ src/Chapter1/Program.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Refactoring.Chapter1
 {
-    class Program
+    public class Program
     {
         static void Main (string[] args)        
         {
---
+++ test/Refactoring.Test.Chapter1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using Refactoring.Chapter1;
+
+namespace Refactoring.Test.Chapter1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+
+            Customer customer = new Customer("Shyam");
+            Movie avengers = new Movie("Avengers", Movie.CHILDREN);
+            Rental rental = new Rental(avengers, 2);
+            customer.AddRental(rental);
+            Assert.That(customer.Statement, Is.EqualTo("Rental record for Shyam\n\tAvengers\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points"));
+
+        }
+    }
+}
