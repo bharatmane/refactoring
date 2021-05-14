7196ea3867f12fe28f83ed457af7c04edeaca2be
bharat.mane <bharat.mane@outlook.com>
2021-05-14 16:43:53 +0530

    test: check the PR

---
+++ src/Chapter1/Dummy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring.Chapter1
+{
+    public class Dummy
+    {
+        private int foo;
+    }
+}
